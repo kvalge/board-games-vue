5e261980f970a26350f9b2206b8e5f177f8091b1
Katrin Valge <katrin.valge@mail.ee>
2022-11-01 14:51:00 +0200

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,86 @@
 <template>
-  <nav>
-    <router-link to="/games">Board Games</router-link> |
-    <router-link to="/new">Edit Products</router-link>
-  </nav>
-  <router-view/>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
+
+<script>
+import TheHeader from '@/components/layout/TheHeader.vue';
+import GamesList from '@/pages/games/GamesList.vue';
+import AddNewGame from "@/pages/new/AddNewGame";
+
+export default {
+  components: {AddNewGame, GamesList, TheHeader},
+}
+
+</script>
+<!--
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/*@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");*/
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+section {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 2rem auto;
+  max-width: 70rem;
+  padding: 1rem;
+  border-radius: 12px;
+}
+
+#app ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+#app li {
+  border-radius: 1px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  width: 20rem;
+  /*margin: 0.4 1rem;*/
+}
+
+#app h1 {
+  font-size: 3rem;
+  color: whitesmoke;
+  margin: 1rem 0 1rem 1rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 2px solid #ccc;
+  color: navy;
+  margin: 0 0 1rem 0;
+}
+
+a {
+  color: navy;
+  font-weight: 600;
 }
 
 nav {
-  padding: 30px;
+  padding: 7px;
+  background-color: navy;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: whitesmoke;
+  font-size: 15px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+-->
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,120 @@
+<template>
+  <header>
+    <nav>
+      <h1>
+        <router-link to="/">Board Games</router-link>
+      </h1>
+      <ul>
+        <li>
+          <router-link to="/games">Games List</router-link>
+        </li>
+        <li>
+          <router-link to="/new">Add New Game</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: navy;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header a {
+  text-decoration: none;
+  color: #f391e3;
+  display: inline-block;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid transparent;
+}
+
+a:active,
+a:hover,
+a.router-link-active {
+  border: 1px solid #f391e3;
+}
+
+h1 {
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+h1 a:hover,
+h1 a:active,
+h1 a.router-link-active {
+  border-color: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 0.5rem;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+<!--
+<template>
+  <header>
+    <h1>{{ name }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['name'],
+}
+</script>
+
+<style scoped>
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: navy;
+  color: white;
+  text-align: center;
+  width: 100%;
+  max-width: 45rem;
+}
+
+p {
+  text-align: center;
+  color: navy;
+}
+</style>-->
